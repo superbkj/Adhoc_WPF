c780db589bff41c0d80afebf1e62ac686361b58c
KG <honda1340916@gmail.com>
2023-03-09 23:38:45 +0900

    first commit

---
+++ TestApp1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnA_Click(object sender, RoutedEventArgs e)
+        {
+            Text1.Text = "ボタンAが押されました";
+        }
+
+        private void BtnB_Click(object sender, RoutedEventArgs e)
+        {
+            Text2.Text = DateTime.Now.ToString("yyyy/MM/dd");
+        }
+    }
+}
